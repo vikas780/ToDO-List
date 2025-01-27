50754263049fd7248d5612f09cd61b5fd510f875
Vikas <vikas1000dhiman@outlook.com>
2025-01-27 18:44:48 +0530

    modify next congig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during builds
+  },
+}
 
-export default nextConfig;
+export default nextConfig
